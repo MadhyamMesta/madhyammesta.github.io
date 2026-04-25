985d61929086aba6918f619e7bcbb578372e2eac
Madhyam Mesta <madhyambmesta@gmail.com>
2026-04-25 20:20:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const elements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+      entry.target.style.transition = "all 0.6s ease";
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
